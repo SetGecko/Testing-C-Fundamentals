70e1b6b25871832749940fb71ad98ca778cb4890
Алексей <setgecko@gmail.com>
2023-06-04 18:09:34 +0300

    Задача 4

---
+++ Task-4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Storage<int>();
+            var b = new Storage<int>();
+            var c = new Storage<string>();
+            Console.WriteLine(a.GetX);
+            Console.WriteLine(b.GetX);
+            Console.WriteLine(c.GetX);
+            Console.Read();
+        }
+    }
+    class Storage<T>
+    {
+        public static int X { get; protected set; }
+        static Storage()
+        {
+            X++;
+        }
+        public int GetX
+            => X;
+    }
+}
