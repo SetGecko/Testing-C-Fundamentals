51d4010e69955bdf665df516954c3dd07940c201
Алексей <setgecko@gmail.com>
2023-06-04 18:25:41 +0300

    Задача 6

---
+++ Task-6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myInt = new MyGeneric<int>();
+            var myStr = new MyGeneric<string>();
+            Console.Read();
+        }
+    }
+    class MyGeneric<T>
+    {
+        static MyGeneric()
+        {
+            Console.WriteLine("I was called");
+        }
+    }
+}
