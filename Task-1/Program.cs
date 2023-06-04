88de6965f120fbc51743147fa6c89ecd7fb45e51
Алексей <setgecko@gmail.com>
2023-06-04 17:56:54 +0300

    Добавьте файлы проекта.

---
+++ Task-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Action<string> action = null;
+                AddMethods(action);
+                action("Hello world");
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+        }
+        static void AddMethods(Action<string> action)
+        {
+            action += Console.WriteLine;
+            action += Console.WriteLine;
+            action -= Console.WriteLine;
+        }
+    }
+}
---
+++ Task-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int iterator = 0;
+            int[] array = { 1, 2, 3, 4, 5, 6};
+            var filter = array.Where(n => n > 3).Select(_ => iterator++);
+            Console.WriteLine(filter.First());
+            Console.WriteLine(iterator);
+            Console.ReadKey();
+        }
+    }
+}
