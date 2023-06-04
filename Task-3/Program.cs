76e967974ee9d1684ee1d5941670b2fe623adfe3
Алексей <setgecko@gmail.com>
2023-06-04 18:05:57 +0300

    Задача 3

---
+++ Task-3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var ms = new MyStruct(5);
+            using (ms)
+            {
+                ms.Foo();
+            }
+            Console.WriteLine(ms.a);
+            Console.Read();
+        }
+    }
+    public struct MyStruct : IDisposable
+    {
+        public int a;
+        public MyStruct(int a)
+        {
+            this.a = a;
+        }
+        public void Foo()
+        {
+            a = 7;
+        }
+        public void Dispose()
+        {
+            a = 0;
+        }
+    }
+}
