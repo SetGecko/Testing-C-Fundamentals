249a5d7e72d53eee23fa14c99faa4b6a65c69ff6
Алексей <setgecko@gmail.com>
2023-06-04 18:14:41 +0300

    Задача 5

---
+++ Task-5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            i.Increment();
+            Console.WriteLine(i);
+            Console.Read();
+        }
+    }
+    public static class MyHelper
+    {
+        public static void Increment(this int i)
+        {
+            i++;
+        }
+    }
+}
